53a634044d9b1c15e0a96526a8e20ea56e81016c
Rush <haidar.abugurain@gmail.com>
2023-02-12 13:50:42 +0300

    add selection sort method

--- Sorter.java
+++ Sorter.java
@@ -6,4 +6,19 @@ public class Sorter {
     public static void main(String[] args) {
         
     }
+
+    public static void selectionSort(int[] array) { 
+        int n = array.length; 
+        for (int i = 0; i < n-1; i++) 
+        { 
+            int index = i; 
+            for (int j = i+1; j < n; j++) 
+                if (array[j] < array[index]) 
+                    index = j; 
+
+            int temp = array[index]; 
+            array[index] = array[i]; 
+            array[i] = temp; 
+        }
+    }
 }
