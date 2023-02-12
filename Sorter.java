28d710686294af1d26e27ae1c9860c6b9f474026
Ali Al Hubail <swe.amh77@gmail.com>
2023-02-12 14:39:14 +0300

    Added test cases to main

--- Sorter.java
+++ Sorter.java
@@ -1,12 +1,25 @@
+import java.util.Arrays;
+
 /**
  * Sorter
  */
 public class Sorter {
 
     public static void main(String[] args) {
-        
+      int[] arr1 = {5, 7, 3, 13, 2};
+      int[] arr2 = {17, 19, 3, 164, 2};
+      System.out.println("Array 1 BEFORE INSERTION SORT");
+      System.out.println(Arrays.toString(arr1));
+      arr1 = sortInsertion(arr1);
+      System.out.println("Array 1 AFTER INESRTION SORT");
+      System.out.println(Arrays.toString(arr1));
+      System.out.println("Array 2 BEFORE SELECTION SORT");
+      System.out.println(Arrays.toString(arr2));
+      System.out.println("Array 2 AFTER SELECTION SORT");
+      arr2 = selectionSort(arr2);
+      System.out.println(Arrays.toString(arr2));
     }
-     public static void sortInsertion(int [] sort_arr){
+     public static int[] sortInsertion(int [] sort_arr){
 
         for(int i=0;i<sort_arr.length;++i){
   
@@ -18,11 +31,11 @@ public class Sorter {
             sort_arr[j] = sort_arr[j-1];
             sort_arr[j-1] = key;
             j = j-1; 
-  
           }
         }
+        return sort_arr;
       }
-    public static void selectionSort(int[] array) { 
+    public static int[] selectionSort(int[] array) { 
         int n = array.length; 
         for (int i = 0; i < n-1; i++) 
         { 
@@ -35,5 +48,6 @@ public class Sorter {
             array[index] = array[i]; 
             array[i] = temp; 
         }
+        return array;
     }
 }
