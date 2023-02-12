98cb02214efefa3181960f00ee5ecffcebeabaa3
Rush <haidar.abugurain@gmail.com>
2023-02-12 14:49:10 +0300

    added test case for selection sort

--- Sorter.java
+++ Sorter.java
@@ -7,11 +7,15 @@ public class Sorter {
 
     public static void main(String[] args) {
       int[] arr1 = {5, 7, 3, 13, 2};
+      int[] arr2 = {12, 18, 2, 9, 5};
       System.out.println("Array 1 BEFORE INSERTION SORT");
       System.out.println(Arrays.toString(arr1));
       arr1 = sortInsertion(arr1);
       System.out.println("Array 1 AFTER INESRTION SORT");
       System.out.println(Arrays.toString(arr1));
+      System.out.println("Array 2 BEFORE SELECTION SORT: " + Arrays.toString(arr2));
+      selectionSort(arr2);
+      System.out.println("Array 2 AFTER SELECTION SORT: " + Arrays.toString(arr2));
     }
      public static int[] sortInsertion(int [] sort_arr){
 
